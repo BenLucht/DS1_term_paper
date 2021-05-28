7aedcd50af6447db883d496281ec0d39a853a4f1
BenLucht <benjamin.b.lucht@gmail.com>
2021-05-28 22:45:51 +0200

    refactor tsne plot, add 3d plot function

--- code/clustering.py
+++ code/clustering.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from kmeans.kmeans import cluster_kmeans
-from plotting.plotting import plot_tsne_2d
+from plotting.plotting import plot_tsne_2d, plot_data_3d
 
 seeds = pd.read_csv('../data/seeds_dataset.txt',
                     sep='\t', 
@@ -10,5 +10,17 @@ seeds = pd.read_csv('../data/seeds_dataset.txt',
 seeds_labeled = cluster_kmeans(seeds[seeds.columns[:-1]], 3, returns='labeled', state=0)
 
 print(seeds_labeled)
+
+plot_data_3d(
+    seeds_labeled[:, :-1], 
+    seeds_labeled[:, -1], 
+    features=[0, 1, 2], 
+    feature_names=['area', 'lenght', 'width'], 
+    title='Seeds', 
+    size=(8, 8), 
+    azimuth=100 , 
+    elevation=40 , 
+    returns='plot'
+  )
 
 plot_tsne_2d(seeds_labeled[:, :-1], seeds_labeled[:, -1], title='Seeds')
--- code/plotting/plotting.py
+++ code/plotting/plotting.py
@@ -2,6 +2,21 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import numpy as np
 
+# list of tuples of (color, marker, label) for each possible cluster
+clusters = [
+  ('r', 'o', 0), 
+  ('b', '^', 1), 
+  ('g', 'x', 2), 
+  ('m', '.', 3), 
+  ('y', '*', 4), 
+  ('c', '+', 5),
+  ('maroon', '>', 6),
+  ('lime', 'D', 7),
+  ('gold', 'H', 8),
+  ('indigo', '1', 9),
+  ('aquamarine', 'v', 10),
+]
+
 def plot_tsne_2d(data, labels, title='', size=(12, 12), returns='plot'):
   '''
   data:     n x m matrix of data (n observations, m features)
@@ -19,16 +34,6 @@ def plot_tsne_2d(data, labels, title='', size=(12, 12), returns='plot'):
   fig = plt.figure(figsize=size)
   ax = fig.add_subplot(111)
 
-  # list of tuples of (color, marker, label) for each possible cluster
-  clusters = [
-    ('r', 'o', 0), 
-    ('b', '^', 1), 
-    ('g', 'x', 2), 
-    ('m', '.', 3), 
-    ('y', '*', 4), 
-    ('cyan', '+', 5)
-  ]
-
   # number of clusters
   n = len(set(labels))
 
@@ -44,6 +49,62 @@ def plot_tsne_2d(data, labels, title='', size=(12, 12), returns='plot'):
     )
 
   ax.set_title(title)
+
+  if returns == 'plot':
+    plt.show()
+  else:
+    return fig
+
+
+def plot_data_3d(
+    data, 
+    labels, 
+    features=[0, 1, 2], 
+    feature_names=['', '', ''], 
+    title='', 
+    size=(12, 12), 
+    azimuth=-60 , 
+    elevation=30 , 
+    returns='plot'
+  ):
+  '''
+  data:           n x m matrix of data (n observations, m features)
+  labels:         label array for all observations
+  features:       list of feature column indices (default first three)
+  feature_names:  list of feature names (default '')
+  title:          title for the plot (default '')
+  size:           size-tuple for the plot (width, height) in inches (default (12,12))
+  azimuth:        angle around z-axis (default -60)
+  elevation:      elevation above 'ground plane' (default 30)
+  returns:        'plot' returns nothing and just plots
+                  'fig' returns figure object
+  '''
+
+  # seems somewhat redundant, but wanted to stay consistent with other functions
+  labeled_data = np.c_[data, labels]
+
+  fig = plt.figure(figsize=size)
+
+  # number of clusters
+  n = len(set(labels))
+
+  ax = fig.add_subplot(111, projection='3d', azim=azimuth, elev=elevation)
+
+  for color, marker, label in clusters[0:n]:
+    current_cluster = labeled_data[labeled_data[:,-1] == label]
+
+    ax.scatter(
+      current_cluster[:, 0], 
+      current_cluster[:, 1], 
+      current_cluster[:, 2], 
+      c=color, 
+      marker=marker
+    )
+
+  ax.set_zlabel(feature_names[0])
+  ax.set_ylabel(feature_names[1])
+  ax.set_xlabel(feature_names[2])
+  ax.set_title('')
 
   if returns == 'plot':
     plt.show()
