058276071714f924f48ba251cb9f96f7f3bf1c79
Jonas <jm.merty@gmail.com>
2021-06-15 09:55:51 +0200

    wip

--- app.py
+++ app.py
@@ -1,37 +1,145 @@
-# pylint: disable=E0611
-# pylint: disable=F0401
-# disables pylint errors for no name in module and unable to import
-
 import streamlit as st
 import pandas as pd
 from code.kmeans.kmeans import cluster_kmeans
 from code.affinity.affinity import cluster_affinity
 from code.meanshift.meanshift import cluster_meanshift
-from code.plotting.plotting import plot_tsne_2d, tsne_transform
-
-# cached wrapper for tsne_transform function
-@st.cache
-def tsne_transform_2(data, state=None):
-    print('transforming')
-    return tsne_transform(data, state=state)
+from code.plotting.plotting import plot_tsne_2d
 
 # LOAD DATA
-seeds = pd.read_csv('data/seeds_dataset.txt', 
-                    sep='\t', 
-                    names=['area', 'perimeter', 'compactness', 'length', 'width', 'asymmetry_coeff', 'length_groove', 'label']
-                   )
-seeds = seeds[seeds.columns[:-1]]
+seeds = pd.read_csv('data/seeds_dataset.txt',
+                    sep='\t',
+                    names=['area', 'perimeter', 'compactness', 'length', 'width', 'asymmetry_coeff', 'length_groove',
+                           'label']
+                    )
+
 customers = pd.read_csv('data/Mall_Customers.csv')
 customers.columns = ['id', 'gender', 'age', 'income', 'spending_score']
+
 housing = pd.read_csv('data/Boston-house-price-data.csv')
 redwine = pd.read_csv('data/winequality-red.csv')
 
-# calculate cached version of tsne transformed data 
-# to speed up interactions
-seeds_tsne = tsne_transform_2(seeds[seeds.columns[:-1]], state=0)
-customers_tsne = tsne_transform_2(customers[['age', 'income', 'spending_score']], state=0)
-housing_tsne = tsne_transform_2(housing, state=0)
-redwine_tsne = tsne_transform_2(redwine, state=0)
+
+@st.cache
+def mydata(arg1):
+    myalgo = arg1
+    return myalgo
+
+@st.cache
+def myalgo(arg1):
+    myalgo = arg1
+    return myalgo
+
+
+
+def kmeans(X, selected_data,  buttontext='Calculate!', j = "", parameter=None):
+    if parameter == None:
+        n = st.sidebar.slider('How many clusters would you like?'+j, 2, 8, 3)
+        st.sidebar.write("I want ", n, 'clusters.')
+
+        if st.sidebar.button(buttontext):
+
+
+            kmeans_labels = cluster_kmeans(X, n, state=0)
+
+            clusters_plot = plot_tsne_2d(X, kmeans_labels, title='kmeans', size=(12, 12), state=0, returns='fig')
+
+            with st.spinner('Plotting data ...'):
+                st.pyplot(clusters_plot)
+                return n
+
+        else:
+            st.write('Please specify your desired parameters.')
+            return n
+    else:
+        kmeans_labels = cluster_kmeans(X, parameter, state=0)
+        clusters_plot = plot_tsne_2d(X, kmeans_labels, title='kmeans', size=(12, 12), state=0, returns='fig')
+        st.pyplot(clusters_plot)
+        return parameter
+
+def meanshift(X, selected_data, buttontext='Calculate!', parameter=None):
+    if parameter ==None:
+         # set bandwidth parameter ranges
+         if selected_data == 'Seeds':
+             min_bandwidth = 1
+             max_bandwidth = 6
+             default_bandwidth = 2
+         elif selected_data == 'Mall Customers':
+             min_bandwidth = 10
+             max_bandwidth = 50
+             default_bandwidth = 22
+         elif selected_data == 'House Pricing':
+             min_bandwidth = 80
+             max_bandwidth = 150
+             default_bandwidth = 120
+         elif selected_data == 'Wine Quality':
+             min_bandwidth = 10
+             max_bandwidth = 40
+             default_bandwidth = 22
+         bw = st.sidebar.slider('Which bandwidth would you like to choose?', min_bandwidth, max_bandwidth, default_bandwidth)
+         st.sidebar.write("I select a bandwidth of ", bw)
+
+         if st.sidebar.button(buttontext):
+
+
+             meanshift_labels = cluster_meanshift(X, bw)
+
+             clusters_plot = plot_tsne_2d(X, meanshift_labels, title='meanshift', size=(12, 12), state=0, returns='fig')
+
+             with st.spinner('Plotting data ...'):
+                 st.pyplot(clusters_plot)
+
+         else:
+             st.write('Please specify your desired parameters.')
+         return bw
+    else:
+        meanshift_labels = cluster_meanshift(X, parameter)
+        clusters_plot = plot_tsne_2d(X, meanshift_labels, title='meanshift', size=(12, 12), state=0, returns='fig')
+        st.pyplot(clusters_plot)
+        return parameter
+
+
+
+def spectralClustering(X, selected_data, buttontext='Calculate!', parameter=None):
+    st.write('Implementation coming soon!')
+    return None
+
+def affinityPropagation(X, selected_data, buttontext='Calculate!', parameter=None):
+    if parameter == None:
+        bw = st.sidebar.slider('Which preference would you like to choose for the comparison?', -10000, -50, -7000)
+        st.sidebar.write("I select a preference of ", bw, "to compare")
+        if st.sidebar.button(buttontext):
+
+            affinity_labes = cluster_affinity(X, bw)
+
+            clusters_plot = plot_tsne_2d(X, affinity_labes, title='', size=(12, 12), state=0, returns='fig')
+
+            with st.spinner('Plotting data ...'):
+                st.pyplot(clusters_plot)
+
+        else:
+            st.write('Please specify your desired parameters.  ')
+        return bw
+    else:
+        affinity_labes = cluster_affinity(X, parameter)
+        clusters_plot = plot_tsne_2d(X, affinity_labes, title='', size=(12, 12), state=0, returns='fig')
+        st.pyplot(clusters_plot)
+        return parameter
+
+
+def getData(selected_data):
+    mydata(selected_data)
+    if selected_data == 'Seeds':
+        X = seeds[seeds.columns[:-1]]
+    elif selected_data == 'Mall Customers':
+        X = customers[['age', 'income', 'spending_score']]
+    elif selected_data == 'House Pricing':
+        X = housing
+    elif selected_data == 'Wine Quality':
+        X = redwine
+
+    return X
+
+
 
 # CONTROLS
 selected_data = st.sidebar.selectbox(
@@ -50,115 +158,69 @@ if selected_data != 'None':
 
     elif option != 'None':
         st.write('You selected:', option)
-        
+        firstinput = 1
+        if option == 'K-Means':
+            firstinput = kmeans(getData(selected_data), selected_data)
+        elif option == 'Mean Shift':
+            firstinput =meanshift(getData(selected_data), selected_data)
+        elif option == 'Spectral Clustering':
+            firstinput =spectralClustering(getData(selected_data), selected_data)
+        elif option == 'Affinity Propagation':
+            firstinput = affinityPropagation(getData(selected_data), selected_data)
+
+        option3 = st.sidebar.selectbox('Which algorithm would you like to compare?',
+                                       ('None', 'Mean Shift', 'K-Means', 'Spectral Clustering', 'Affinity Propagation'))
+
+        if option3 != "None":
+
+
+            if option3 == 'K-Means':
+                if option == 'K-Means':
+                    firstinput = kmeans(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Mean Shift':
+                    firstinput = meanshift(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Spectral Clustering':
+                    firstinput = spectralClustering(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Affinity Propagation':
+                    firstinput = affinityPropagation(getData(selected_data), selected_data, parameter=firstinput)
+                kmeans(getData(selected_data), selected_data, "Compare?")
+
+            elif option3 == 'Mean Shift':
+                if option == 'K-Means':
+                    firstinput = kmeans(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Mean Shift':
+                    firstinput = meanshift(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Spectral Clustering':
+                    firstinput = spectralClustering(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Affinity Propagation':
+                    firstinput = affinityPropagation(getData(selected_data), selected_data, parameter=firstinput)
+                meanshift(getData(selected_data), selected_data, "Compare?")
+
+            elif option3 == 'Spectral Clustering':
+                if option == 'K-Means':
+                    firstinput = kmeans(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Mean Shift':
+                    firstinput = meanshift(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Spectral Clustering':
+                    firstinput = spectralClustering(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Affinity Propagation':
+                    firstinput = affinityPropagation(getData(selected_data), selected_data, parameter=firstinput)
+                spectralClustering(getData(selected_data), selected_data, "Compare?")
+            elif option3 == 'Affinity Propagation':
+                if option == 'K-Means':
+                    firstinput = kmeans(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Mean Shift':
+                    firstinput = meanshift(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Spectral Clustering':
+                    firstinput = spectralClustering(getData(selected_data), selected_data, parameter=firstinput)
+                elif option == 'Affinity Propagation':
+                    firstinput = affinityPropagation(getData(selected_data), selected_data, parameter=firstinput)
+
+                affinityPropagation(getData(selected_data), selected_data, "Compare?")
+
+
+
+
 else:
     st.write('Please select a dataset.')
 
-
-# LOGIC
-if option == 'K-Means':
-
-    n = st.sidebar.slider('How many clusters would you like?', 2, 8, 3)
-    st.sidebar.write("I want ", n, 'clusters.')
-
-    if st.sidebar.button('Calculate!'):
-        if selected_data == 'Seeds':
-            X = seeds
-            X_plot = seeds_tsne
-        elif selected_data == 'Mall Customers':
-            X = customers[['age', 'income', 'spending_score']]
-            X_plot = customers_tsne
-        elif selected_data == 'House Pricing':
-            X = housing
-            X_plot = housing_tsne
-        elif selected_data == 'Wine Quality':
-            X = redwine
-            X_plot = redwine_tsne
-
-        kmeans_labels = cluster_kmeans(X, n, state=0)
-
-        clusters_plot = plot_tsne_2d(X_plot, kmeans_labels, title='', size=(12, 12), state=0, returns='fig')
-
-        with st.spinner('Plotting data ...'):
-            st.pyplot(clusters_plot)
-
-    else:
-        st.write('Please specify your desired parameters.')
-
-elif option == 'Mean Shift':
-    # set bandwidth parameter ranges
-    if selected_data == 'Seeds':
-        min_bandwidth = 1
-        max_bandwidth = 6
-        default_bandwidth = 2
-    elif selected_data == 'Mall Customers':
-        min_bandwidth = 10
-        max_bandwidth = 50
-        default_bandwidth = 22
-    elif selected_data == 'House Pricing':
-        min_bandwidth = 80
-        max_bandwidth = 150
-        default_bandwidth = 120
-    elif selected_data == 'Wine Quality':
-        min_bandwidth = 10
-        max_bandwidth = 40
-        default_bandwidth = 22
-
-    bw = st.sidebar.slider('Which bandwidth would you like to choose?', min_bandwidth, max_bandwidth, default_bandwidth)
-    st.sidebar.write("I select a bandwidth of ", bw)
-
-    if st.sidebar.button('Calculate!'):
-        if selected_data == 'Seeds':
-            X = seeds
-            X_plot = seeds_tsne
-        elif selected_data == 'Mall Customers':
-            X = customers[['age', 'income', 'spending_score']]
-            X_plot = customers_tsne
-        elif selected_data == 'House Pricing':
-            X = housing
-            X_plot = housing_tsne
-        elif selected_data == 'Wine Quality':
-            X = redwine
-            X_plot = redwine_tsne
-
-        meanshift_labels = cluster_meanshift(X, bw)
-
-        clusters_plot = plot_tsne_2d(X_plot, meanshift_labels, title='', size=(12, 12), state=0, returns='fig')
-
-        with st.spinner('Plotting data ...'):
-            st.pyplot(clusters_plot)
-
-    else:
-        st.write('Please specify your desired parameters.')
-
-
-elif option == 'Spectral Clustering':
-  st.write('Implementation coming soon!')
-
-elif option == 'Affinity Propagation':
-    bw = st.sidebar.slider('Which preference would you like to choose?', -10000, -50, -7000)
-    st.sidebar.write("I select a preference of ", bw)
-
-    if st.sidebar.button('Calculate!'):
-        if selected_data == 'Seeds':
-            X = seeds
-            X_plot = seeds_tsne
-        elif selected_data == 'Mall Customers':
-            X = customers[['age', 'income', 'spending_score']]
-            X_plot = customers_tsne
-        elif selected_data == 'House Pricing':
-            X = housing
-            X_plot = housing_tsne
-        elif selected_data == 'Wine Quality':
-            X = redwine
-            X_plot = redwine_tsne
-
-        affinity_labels = cluster_affinity(X, bw)
-
-        clusters_plot = plot_tsne_2d(X_plot, affinity_labels, title='', size=(12, 12), state=0, returns='fig')
-
-        with st.spinner('Plotting data ...'):
-            st.pyplot(clusters_plot)
-
-    else:
-        st.write('Please specify your desired parameters.  ')
